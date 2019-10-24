ad6af64e3a58ff933221027ee057662197110277
amallik-ualberta <amallik@ualberta.ca>
2019-10-24 14:03:51 -0600

    writing output of nltk tagger to file

--- pos_nltk.py
+++ pos_nltk.py
@@ -4,108 +4,114 @@ from nltk.tag import *
 
 from nltk.tbl.template import Template
 
-
+from nltk.tag.brill import Pos, Word
 
 import re
 
+def pre_process(filename):
 
-f = open("A3DataCleaned/Domain2Train.txt","r")
+	f = open(filename, "r")
 
-contents = f.read()
+	contents = f.read()
 
-contents = re.compile("\n").split(contents)
+	contents = re.compile("\n").split(contents)
 
-train_data = []
+	data = []
 
-train_data_line = []
-
-
-
-for line in contents:
-
-	if not line:
-		
-		
-
-		
-		train_data.append (train_data_line)
-
-		train_data_line = []
-
-	else:
-		
-
-		word_and_tag = re.compile("[ ]+").split(line)
-
-		if (len(word_and_tag)!=2):
-
-			print(word_and_tag)
-
-		word_and_tag = tuple(word_and_tag)
-
-		train_data_line.append(word_and_tag)
-
-		
+	data_line = []
 
 
 
+	for line in contents:
+
+		if not line:
+			
+			
+
+			
+			data.append (data_line)
+
+			data_line = []
+
+		else:
+			
+
+			word_and_tag = re.compile("[ ]+").split(line)
+
+			if (len(word_and_tag)!=2):
+
+				print(word_and_tag)
+
+			word_and_tag = tuple(word_and_tag)
+
+			data_line.append(word_and_tag)
+
+
+	return data;
+
+
+def write_output (output_file, test_data, tagger):
+
+
+	f = open (output_file, "w")
+
+	for line in test_data:
+
+
+		sentence = []
+
+		for pair in line :
+
+			sentence.append(pair[0])
+
+
+		word_and_tag = tagger.tag(sentence)
+
+		for pair in word_and_tag :
+
+			f.write(pair[0]+ " " + pair[1] + "\n")
+
+		f.write("\n")
 
 
 
-f = open("A3DataCleaned/Domain1Test.txt", "r")
+def main():	
 
-contents = f.read()
 
-contents = re.compile("\n").split(contents)
+	train_file = "Domain2Train.txt"
+	
 
-test_data = []
+	train_data = pre_process (train_file)
 
-test_data_line = []
 
-for line in contents:
+	test_file = "Domain1Test.txt"
 
-	if not line:
-		
-		
 
-		test_data.append (test_data_line)
+	test_data = pre_process (test_file)
 
-		test_data_line = []
+	
+	hmm_tagger = hmm.HiddenMarkovModelTagger.train(train_data,test_data)
 
-	else:
-		
+	output_file = "output_hmm_" + train_file + "_" + test_file
 
-		if (len(word_and_tag)!=2):
+	write_output(output_file, test_data, hmm_tagger)
+	
 
-			print(word_and_tag)
 
-		word_and_tag = re.compile("[ ]+").split(line)
-
-		word_and_tag = tuple(word_and_tag)
-
-		test_data_line.append(word_and_tag)
-
-hmm_tagger = hmm.HiddenMarkovModelTagger.train(train_data,test_data)
+	Template._cleartemplates() #clear any templates created in earlier tests
 
 
 
-#sentence = ["lone" ,"wolf", "dies", "and", "the", "pack", "survives"]
+	brill_trainer = BrillTaggerTrainer(hmm_tagger, brill.brill24())
 
-#print(hmm_tagger.tag(sentence))
+	brill_tagger = brill_trainer.train(train_data)
 
-#print(hmm_tagger.evaluate(test_data))
+	print(brill_tagger.evaluate(test_data))
 
-Template._cleartemplates() #clear any templates created in earlier tests
-
-
-
-brill_trainer = BrillTaggerTrainer(hmm_tagger, brill.brill24())
-
-brill_tagger = brill_trainer.train(train_data)
-
-print(brill_tagger.evaluate(test_data))
-
-#brill_tagger.evaluate(test_data)
+	#brill_tagger.evaluate(test_data)
 
 
 
+
+if __name__ == "__main__":
+    main()
